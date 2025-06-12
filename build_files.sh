ebadf6ddad53be35e0099586b027e12e52a09229
xtracut-Arun <arun@xtracut.com>
2025-06-12 13:14:37 +0530

    trying to run collectstatic with .sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,22 @@
-#!/bin/bash
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear 
+# Check the installed Python version and adjust the command accordingly
+PYTHON_VERSION=$(python3 --version)
+if [[ $PYTHON_VERSION == *"3.12"* ]]; then
+    PYTHON_COMMAND="python3.12"
+elif [[ $PYTHON_VERSION == *"3.11"* ]]; then
+    PYTHON_COMMAND="python3.11"
+elif [[ $PYTHON_VERSION == *"3.9"* ]]; then
+    PYTHON_COMMAND="python3.9"
+else
+    PYTHON_COMMAND="python3"  # Fallback to default Python 3 version
+fi
+
+  
+# Replace `python3.11.5` with `$PYTHON_COMMAND`
+$PYTHON_COMMAND manage.py collectstatic --noinput
+
+$PYTHON_COMMAND -m pip install -r requirements.txt
+# Create the output directory if it doesn't exist
+mkdir -p staticfiles_build
+
+# Move the static files into the output directory
+mv static staticfiles_build
